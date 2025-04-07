11ad38494f42a93718c6aa3a7618c097a21d9e33
Nohemi200712 <Nohemigeronimochable@gmail.com>
2025-04-07 17:30:13 -0600

    Add files via upload
    examen

---
+++ ejemplo.js
@@ -0,0 +1,11 @@
+function saludar ()
+{
+let saludar=prompt("ingresar nombre");
+alert("Hola "+saludar+"suerte en tu examen");
+}
+function calcularCuadrado ()
+{
+let numero=parseFloat(prompt("ingresa el numero que desea calcular"));
+let cuadrado=(numero*numero);
+alert ("El cuadrado de un numero es "+cuadrado);
+}
