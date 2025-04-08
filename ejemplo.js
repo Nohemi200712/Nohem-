e303ee7abb1da1d73391a8b03462e0b8fd1a29d1
Nohemi200712 <Nohemigeronimochable@gmail.com>
2025-04-08 12:08:31 -0600

    Add files via upload

--- ejemplo.js
+++ ejemplo.js
@@ -1,11 +1,12 @@
-function saludar ()
+function saludar()
 {
-let saludar=prompt("ingresar nombre");
-alert("Hola "+saludar+"suerte en tu examen");
+    let saludar1=prompt("ingrese el nombre");
+    alert("Hola "+saludar1+ " suerte en tu examen");
 }
+
 function calcularCuadrado ()
 {
-let numero=parseFloat(prompt("ingresa el numero que desea calcular"));
-let cuadrado=(numero*numero);
-alert ("El cuadrado de un numero es "+cuadrado);
+    let numcuadrado=parseFloat(prompt("escribe el numero que desea calcular"));
+    let cuadrado= (numcuadrado*numcuadrado);
+    alert ("El cuadrado del número es "+cuadrado);
 }
